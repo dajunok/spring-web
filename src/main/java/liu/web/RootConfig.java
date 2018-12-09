d5ae22fe18d1f745ee20421f953a0dfe72a05b0c
liulijun <1937249556@qq.com>
2018-12-09 20:03:21 +0800

    初次提交：liu.web

---
+++ src/main/java/liu/web/RootConfig.java
@@ -0,0 +1,16 @@
+package liu.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.ComponentScan.Filter;
+
+@Configuration
+@ComponentScan(basePackages= {"liu.web"},
+		excludeFilters= {
+				@Filter(type=FilterType.ANNOTATION,value=EnableWebMvc.class)
+		})
+public class RootConfig {
+
+}
---
+++ src/main/java/liu/web/Test.java
@@ -0,0 +1,18 @@
+package liu.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class Test {
+	@org.junit.Test
+	public void testHomePage() throws Exception {
+		HomeController controller=new HomeController();
+		MockMvc mockMvc=MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/")).andExpect(view().name("home"));		
+	}
+	
+
+}
